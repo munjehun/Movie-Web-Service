290cdf46e1a96a836a35ae7eef97886a8418eaa6
munjehun <miso1489@gmail.com>
2022-09-14 00:47:25 +0900

    props, create-react-app

---
+++ movie-web-service/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button.js";
+
+// index.js로 보내서 index.js에서 출력!
+function App() {
+  return (
+    <div className="App">
+      <h1>리액트 복습 시작!!</h1>
+      <Button text={"가즈아아아"} />
+    </div>
+  );
+}
+
+export default App;
